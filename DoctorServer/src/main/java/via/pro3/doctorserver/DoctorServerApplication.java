4d1e39cf95d2d1d02899d0d206e2f7f7178fc8ce
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-10-23 11:51:09 +0200

    DoctorServer

---
+++ DoctorServer/src/main/java/via/pro3/doctorserver/DoctorServerApplication.java
@@ -0,0 +1,14 @@
+package via.pro3.doctorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class DoctorServerApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(DoctorServerApplication.class, args);
+  }
+
+}
