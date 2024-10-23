4566ac5a7b9defa93482e3197fe325955d9e8a99
SaibotGames <mkbarczuk@wp.pl>
2024-10-23 15:21:27 +0200

    w

--- DoctorServer/src/main/java/via/pro3/doctorserver/DoctorServerApplication.java
+++ DoctorServer/src/main/java/via/pro3/doctorserver/DoctorServerApplication.java
@@ -3,11 +3,23 @@ package via.pro3.doctorserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication public class DoctorServerApplication
 {
 
   public static void main(String[] args)
   {
+    try {
+      // Retrieve and print the local host IP address
+      InetAddress localHost = InetAddress.getLocalHost();
+      String ipAddress = localHost.getHostAddress();
+      System.out.println("Server is starting...");
+      System.out.println("Server IP Address: " + ipAddress);
+    } catch (UnknownHostException e) {
+      System.err.println("Unable to retrieve IP address: " + e.getMessage());
+    }
     SpringApplication.run(DoctorServerApplication.class, args);
   }
 
