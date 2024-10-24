4a2f2ae709be305ef9be1f0553f229e23290f28a
Danny <>
2024-10-24 16:32:38 +0200

    situationship

--- DoctorServer/src/main/java/via/pro3/doctorserver/Server.java
+++ DoctorServer/src/main/java/via/pro3/doctorserver/Server.java
@@ -1,22 +1,34 @@
 package via.pro3.doctorserver;
-
-import org.springframework.http.ResponseEntity;
+import example.grpc.GreeterGrpc;
+import example.grpc.HelloReply;
+import example.grpc.HelloRequest;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
-
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/doctorname")
 
-public class Server
-{
-  @GetMapping("/name")
-  public String getDoctorName(@RequestParam String name)
-  {
-    System.out.println("Client connected at: " + LocalDateTime.now() + " - Received name: " + name);
+public class Server {
+  private GreeterGrpc.GreeterBlockingStub blockingStub;
+  public Server() {
+    ManagedChannel channel = ManagedChannelBuilder
+            .forAddress("localhost", 9090)
+            .usePlaintext()
+            .build();
 
-    return "Hello, " + name;
+    blockingStub = GreeterGrpc.newBlockingStub(channel);
   }
 
+  @GetMapping("/name")
+  public String getDoctorName(@RequestParam String name) {
+    System.out.println("Client connected at: " + LocalDateTime.now() + " - Received name: " + name);
+    HelloRequest request = HelloRequest.newBuilder()
+            .setName(name)
+            .build();
+
+    HelloReply response = blockingStub.sayHello(request);
+    return "Hello, " + response.getMessage();
+  }
 }
--- MainServer/src/main/java/via/pro3/mainserver/DatabaseSingleton.java
+++ MainServer/src/main/java/via/pro3/mainserver/DatabaseSingleton.java
@@ -11,7 +11,7 @@ public class DatabaseSingleton {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres?currentSchema=sep3";
     private static final String USER = "postgres";
-    private static final String PSWD = "VIAVIAVIA";
+    private static final String PSWD = "admin";
 
     private DatabaseSingleton(){
         Connection conn = null;
