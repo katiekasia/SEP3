5c898e6b131b45040669ee8c048886f9123eae84
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-10-29 14:47:45 +0100

    PatientServer

---
+++ PatientServer/src/main/java/via/pro3/patientserver/PatientServerApplication.java
@@ -0,0 +1,14 @@
+package via.pro3.patientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class PatientServerApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(PatientServerApplication.class, args);
+  }
+//new
+}
---
+++ PatientServer/src/test/java/via/pro3/patientserver/PatientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package via.pro3.patientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class PatientServerApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
