933732db6482868fa1bc317631adb17ab88ff870
DannyMnt <munteanudaniel098@gmail.com>
2024-11-13 10:49:48 +0100

    test

--- MainServer/src/main/java/via/pro3/mainserver/database/EventRepository.java
+++ MainServer/src/main/java/via/pro3/mainserver/database/EventRepository.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.sql.SQLException;
 
 public class EventRepository implements EventInterface {
     private DatabaseInterface database;
