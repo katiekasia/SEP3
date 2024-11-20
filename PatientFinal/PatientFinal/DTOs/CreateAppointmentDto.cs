053fa2a67dd5ec2a058b1fcb0528734633006a44
katiekasia <olejarczykkatarzyna2022@gmail.com>
2024-11-20 10:44:58 +0100

    changes on patient client blazor

--- PatientFinal/PatientFinal/DTOs/CreateAppointmentDto.cs
+++ PatientFinal/PatientFinal/DTOs/CreateAppointmentDto.cs
@@ -7,7 +7,7 @@ public class CreateAppointmentDto
         
        // public DateTime Date { get; set; }
        // public TimeSpan Time { get; set; }
-      // public string Specialization { get; set; }
+       public string Specialization { get; set; }
        public string City { get; set; }
         public string Status { get; set; }
         public string Description { get; set; }
