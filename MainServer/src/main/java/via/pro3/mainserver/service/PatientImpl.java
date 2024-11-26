5ae50630b8f9c1f4838f2594f7de1594548da990
SaibotGames <mkbarczuk@wp.pl>
2024-11-26 10:28:30 +0100

    imports

--- MainServer/src/main/java/via/pro3/mainserver/service/PatientImpl.java
+++ MainServer/src/main/java/via/pro3/mainserver/service/PatientImpl.java
@@ -4,18 +4,8 @@ import io.grpc.stub.StreamObserver;
 import createBooking.grpc.CreateAppointment;
 import createBooking.grpc.PatientBookingGrpc;
 import createBooking.grpc.DBresponse;
-import org.springframework.aop.support.AbstractPointcutAdvisor;
 import via.pro3.mainserver.DTOs.CreateAppointmentDto;
 import via.pro3.mainserver.Model.*;
-import via.pro3.mainserver.database.DatabaseInterface;
-import via.pro3.mainserver.database.DatabaseSingleton;
-import via.pro3.mainserver.database.EventInterface;
-import via.pro3.mainserver.database.EventRepository;
-
-import java.sql.Time;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Random;
 
 public class PatientImpl extends PatientBookingGrpc.PatientBookingImplBase {
     private final Model model;
