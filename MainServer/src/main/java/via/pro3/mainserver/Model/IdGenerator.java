dc236ad91e148f64c1a78fa707ddb8345b69487d
DannyMnt <munteanudaniel098@gmail.com>
2024-12-11 00:48:06 +0100

    sorry michal

--- MainServer/src/main/java/via/pro3/mainserver/Model/IdGenerator.java
+++ MainServer/src/main/java/via/pro3/mainserver/Model/IdGenerator.java
@@ -3,46 +3,62 @@ package via.pro3.mainserver.Model;
 import java.util.HashSet;
 import java.util.Set;
 
-public class IdGenerator implements GeneratorInterface
-{
-  private Set<Integer> appointmentIds;
-  private Set<Integer> prescriptionIds;
+public class IdGenerator implements GeneratorInterface {
+    private Set<Integer> appointmentIds;
+    private Set<Integer> prescriptionIds;
 
-  public IdGenerator(){
-    appointmentIds = new HashSet<Integer>();
-    prescriptionIds = new HashSet<Integer>();
-  }
-  @Override public int generateAppointmentId()
-  {
-    int nextId = appointmentIds.size();
-    appointmentIds.add(nextId);
-    return nextId;
-  }
+    public IdGenerator(int appointmentCount, int prescriptionCount) {
+        appointmentIds = new HashSet<>();
+        prescriptionIds = new HashSet<>();
 
-  @Override public int generatePrescriptionId()
-  {
-    int nextId = prescriptionIds.size();
-    addPrescriptionId(nextId);
-    return nextId;
-  }
+        for (int i = 0; i < appointmentCount; i++) {
+            appointmentIds.add(i);
+        }
+        for (int i = 0; i < prescriptionCount; i++) {
+            prescriptionIds.add(i);
+        }
+    }
 
-  @Override public void addAppointmentId(int id)
-  {
-    appointmentIds.add(id);
-  }
+    @Override
+    public int generateAppointmentId() {
+        int nextId = appointmentIds.size();
+        appointmentIds.add(nextId);
+        return nextId;
+    }
 
-  @Override public void addPrescriptionId(int id)
-  {
-    prescriptionIds.add(id);
-  }
 
-  @Override public void setAppointmentIds(Set<Integer>  ids)
-  {
-appointmentIds = ids;
-  }
+    @Override
+    public int generatePrescriptionId() {
+        int nextId = getNextAvailableId(prescriptionIds);
+        prescriptionIds.add(nextId);
+        return nextId;
+    }
 
-  @Override public void setPrescriptionIds(Set<Integer>  ids)
-  {
-prescriptionIds = ids;
-  }
+    private int getNextAvailableId(Set<Integer> ids) {
+        int nextId = 0;
+        while (ids.contains(nextId)) {
+            nextId++;
+        }
+        return nextId;
+    }
+
+    @Override
+    public void addAppointmentId(int id) {
+        appointmentIds.add(id);
+    }
+
+    @Override
+    public void addPrescriptionId(int id) {
+        prescriptionIds.add(id);
+    }
+
+    @Override
+    public void setAppointmentIds(Set<Integer> ids) {
+        appointmentIds = ids;
+    }
+
+    @Override
+    public void setPrescriptionIds(Set<Integer> ids) {
+        prescriptionIds = ids;
+    }
 }
